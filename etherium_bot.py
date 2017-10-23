ce73acbb3d745fabb1b76750e5663202a578ac06
Kuba Suder <jakub.suder@gmail.com>
2017-10-23 14:29:15 +0000

    ignore comments mentioning the bot

--- etherium_bot.py
+++ etherium_bot.py
@@ -9,7 +9,7 @@ import time
 reddit = praw.Reddit('etheriumbot')
 
 required_words = ['etherium', 'ethereium', 'etharium', 'entherium']
-banned_words = ['ethereum', 'spell', 'spelt', 'write', 'wrote', 'written', 'type', 'typo', 'master', 'astrolabe', 'sculptor', 'reach']
+banned_words = ['ethereum', 'spell', 'spelt', 'write', 'wrote', 'written', 'type', 'typo', 'master', 'astrolabe', 'sculptor', 'reach', 'etherium_bot', 'etherium\\_bot', 'etherium bot']
 
 banned_subreddits = ['bravenewbies', 'budgetdecks', 'casualmtg', 'competitiveedh', 'custommagic', 'dragonvale', 'dust514', 'edh', 'enairim', 'eve', 'evedreddit', 'evejobs', 'evememes', 'evenewbies', 'eveonline', 'eveporn', 'fittings', 'lrcast', 'magiccardpulls', 'magicdeckbuilding', 'magicduels', 'magictcg', 'modernmagic', 'mtgaltered', 'mtgcube', 'mtgfinance', 'mtggore', 'mtgjudge', 'mtglimited', 'mtgo', 'mtgporn', 'oblivionmods', 'pauper', 'perkusmaximus', 'skyrim', 'skyrimmod_jp', 'skyrimmods', 'skyrimporn', 'skyrimrequiem', 'skywind', 'spikes', 'xedit']
 
